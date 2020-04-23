16a4afbac8dd859859bee2171cc3a300f43acb8e
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-22 22:25:34 -0400

    created home component

---
+++ src/components/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+export default class Home extends React.Component{
+  
+  render(){
+    return (
+    <div>
+
+    </div>
+
+  );
+    }
+}
+
+export default App;
