126bf75b3e26e74b14424278cc974a3f2c538436
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-22 23:24:16 -0400

    router fully implemented besides link now the creation starts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import Home from './components/home';
+import Horror from './components/horror';
+import About from './components/about';
+import Comedy from './components/comedy';
+import Drama from './components/drama';
+import Action from './components/action';
+
 
 function App() {
   return (
-    <Router>
-   
-    </Router>
+  <Router>
+    <Switch>
+     <Route path="/" component={Home} />
+    <Route path="/Home" component={Home} />
+    <Route path="/About" component={About} />
+    <Route path="/Comedy" component={Comedy} />
+    <Route path="/Horror" component={Horror} />
+    <Route path="/Action" component={Action} />
+    <Route path="/Drama" component={Drama} /> 
+    </Switch>
+  </Router>
   );
 }
 
--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,12 @@ import React from 'react';
 import '../';
 
 export default class Home extends React.Component{
-  
+  constructor(props){
+      super(props)
+      this.state = {
+          background: ''
+      }
+  }
   render(){
     return (
     <div id="main-nav">
