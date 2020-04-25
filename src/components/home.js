a9a11a4dc802383fa54e797d4729fb6a6d49110d
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-24 22:10:54 -0400

    set userQuery to empty string

--- src/components/home.js
+++ src/components/home.js
@@ -9,7 +9,7 @@ export default class Home extends React.Component{
       super(props)
       this.state = {
           results: [],
-          userQuery: ''
+          userQuery: ""
       }
      } 
 
