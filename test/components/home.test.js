d9214f35cf21926f77bafcad0f5efdaebabe0488
Charles <charles.lamb.dev@gmail.com>
2023-08-14 22:37:26 -0400

    added the tests for the home page

--- test/components/home.test.js
+++ test/components/home.test.js
@@ -9,3 +9,53 @@ const Home = require('..components/Home');
 const { window } = new JSDOM('<!doctype html><html><body></body></html>');
 global.window = window;
 global.document = window.document;
+
+describe('Home Component', function () {
+    let wrapper;
+  
+    before(function () {
+      wrapper = mount(<Home />);
+    });
+  
+    it('Popup should be initially visible', function () {
+      const popupDiv = wrapper.find('#popupDiv');
+      assert.isTrue(popupDiv.exists());
+      assert.equal(popupDiv.prop('style').visibility, 'visible');
+    });
+  
+    it('handleInputChange should update userQuery state', function () {
+      const event = { target: { value: 'testQuery' } };
+      wrapper.instance().handleInputChange(event);
+      assert.equal(wrapper.state('userQuery'), 'testQuery');
+    });
+  
+    it('handleGetShow should update results state after API call', async function () {
+      // Mock axios.get to return a response
+      const mockResponse = {
+        data: {
+          tv_shows: [
+            {
+              name: 'Test Show',
+              network: 'Test Network',
+              start_date: '2023-01-01',
+              status: 'Running',
+            },
+          ],
+        },
+      };
+      const axios = require('axios');
+      axios.get = () => Promise.resolve(mockResponse);
+  
+      const event = { preventDefault: () => {} };
+      await wrapper.instance().handleGetShow(event);
+  
+      const results = wrapper.state('results');
+      assert.deepEqual(results, {
+        name: 'Test Show',
+        network: 'Test Network',
+        start_date: '2023-01-01',
+        status: 'Running',
+      });
+    });
+
+});
