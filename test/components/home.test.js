87c6451c5b5f33ad1884ef5dcc77143a72f90975
Charles <charles.lamb.dev@gmail.com>
2023-08-14 20:58:03 -0400

    added imports to the test file and installed enzyme in my local environment

---
+++ test/components/home.test.js
@@ -0,0 +1,6 @@
+// Import the necessary modules and libraries
+const assert = require('chai').assert;
+const { JSDOM } = require('jsdom');
+const React = require('react');
+const { mount } = require('enzyme');
+const Home = require('./Home');
