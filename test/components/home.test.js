469da3fbc10a3516a336045d1752b11db140ebe3
Charles <charles.lamb.dev@gmail.com>
2023-08-14 22:33:02 -0400

    added the code to put the react page into a variable via jsdom

--- test/components/home.test.js
+++ test/components/home.test.js
@@ -5,3 +5,7 @@ const React = require('react');
 const { mount } = require('enzyme');
 const Home = require('..components/Home');
 
+// Simulate a DOM environment using JSDOM
+const { window } = new JSDOM('<!doctype html><html><body></body></html>');
+global.window = window;
+global.document = window.document;
